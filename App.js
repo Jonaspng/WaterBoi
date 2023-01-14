a03c6c90bdfcdf3ea473b6e97132901f2126678d
Jonaspng <jonaspng@hotmail.com>
2023-01-14 14:08:47 +0800

    Add paper provider

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { HomeIcon, DocumentIcon } from 'react-native-heroicons/solid';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 import HomeScreen from './screens/HomeScreen';
 import AboutScreen from './screens/AboutScreen';
@@ -11,21 +12,24 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name='Home' component={HomeScreen} options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: () => (
-            <HomeIcon />
-          )
-        }}/>
-        <Tab.Screen name='About' component={AboutScreen} options={{
-          tabBarLabel: 'About',
-          tabBarIcon: () => (
-            <DocumentIcon />
-          )
-        }}/>
-      </Tab.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name='Home' component={HomeScreen} options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: () => (
+              <HomeIcon />
+            )
+          }}/>
+          <Tab.Screen name='About' component={AboutScreen} options={{
+            tabBarLabel: 'About',
+            tabBarIcon: () => (
+              <DocumentIcon />
+            )
+          }}/>
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+    
   );
 }
