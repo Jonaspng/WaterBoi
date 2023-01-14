bd5661b34bcd3f6e9b57b200d06804dd2023dc9e
Jonaspng <jonaspng@hotmail.com>
2023-01-14 14:22:55 +0800

    Add back app.js

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ import { Provider as PaperProvider } from 'react-native-paper';
 
 import HomeScreen from './screens/HomeScreen';
 import AboutScreen from './screens/AboutScreen';
-import TodayRecord from './components/TodayRecord';
 
 // const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -14,7 +13,22 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <PaperProvider>
-      <TodayRecord />
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name='Home' component={HomeScreen} options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: () => (
+              <HomeIcon />
+            )
+          }}/>
+          <Tab.Screen name='About' component={AboutScreen} options={{
+            tabBarLabel: 'About',
+            tabBarIcon: () => (
+              <DocumentIcon />
+            )
+          }}/>
+        </Tab.Navigator>
+      </NavigationContainer>
     </PaperProvider>
     
   );
